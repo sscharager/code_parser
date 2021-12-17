a51384a591efec5a3693db91a97d112e99bbf1d3
sscharager <96275563+sscharager@users.noreply.github.com>
2021-12-16 22:27:32 -0500

    Update compiler.h

--- compiler.h
+++ compiler.h
@@ -1,9 +1,3 @@
-/* 
-	This is the header file for the UCF Fall 2021 Systems Software Project.
-	If you choose to alter this, you MUST make a note of that in your
-	readme file, otherwise you will lose 5 points.
-*/
-
 typedef enum token_type {
 	constsym = 1, varsym, procsym, dosym, odsym, whilesym, whensym, 
 	elsedosym, callsym, writesym, readsym, identsym, numbersym, assignsym, 
